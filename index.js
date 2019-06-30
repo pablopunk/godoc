adb6c7e1dd1fe8997f69cef88a1390da2e1f813d
pablopunk <pablovarela182@gmail.com>
2019-06-30 04:48:13 +0200

    Simplify

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch')
 
 module.exports = async (req, res) => {
-  const html = (await (await fetch('https://github.com' + req.url)).text())
+  const res = await fetch(`https://github.com${req.url}`)
+  const html = await res.text()
     .replace(/(href=.)https?:\/\/github.com/g, '$1//' + req.headers.host)
     .replace(
       '</head>',
       `<link media="all" rel="stylesheet" href="/styles.css" />
-      ${req.query.dark !== undefined ? '<link media="all" rel="stylesheet" href="/dark.css" />' : ''}
-      `
+      ${req.query.dark !== undefined ? '<link media="all" rel="stylesheet" href="/dark.css" />' : ''}`
     )
 
   res.end(html)
