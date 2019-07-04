37f07c599c5da950773b7cf9984da5776dd2b051
pablopunk <pablovarela182@gmail.com>
2019-07-04 16:52:54 +0200

    Don't redirect links to github

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fetch = require('node-fetch')
 module.exports = async (req, res) => {
   const githubRes = await fetch(`https://github.com${req.url}`)
   const html = (await githubRes.text())
-    .replace(/(href=.)https?:\/\/github.com/g, '$1//' + req.headers.host)
     .replace(
       '</head>',
       `<link media="all" rel="stylesheet" href="/styles.css" />
